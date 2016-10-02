5bcd065d27364106cd54244973eb9f7a8befd5b7
aislinnozawaburns <aislinnozawaburns@Aislinns-MacBook-Pro.local>
2016-10-02 13:32:33 -0700

    remove place holder in body

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//submit new task//
 var newTask = document.querySelector("#new-task");
 var newTaskBody = document.querySelector("#new-task-body");
 var tasks = document.querySelector("#tasks");
@@ -8,15 +9,37 @@ function addTask(event) {
   event.preventDefault();
 
 var task = document.createElement("div");
+var li = document.createElement("li");
+var label = document.createElement("label");
+var input = document.createElement("input");
+input.type = "checkbox";
+input.class = "checkbox"
+var span = document.createElement("span");
 
 task.classList.add("task");
+li.classList.add("li");
+label.classList.add("label");
+input.classList.add("input");
+span.classList.add("span");
 
-task.textContent = newTaskBody.value;
+
+span.textContent = newTaskBody.value;
 
 tasks.appendChild(task);
-
+task.appendChild(li);
+li.appendChild(label);
+label.appendChild(input);
+label.appendChild(span);
 newTask.reset();
+
+var placeHolderInList = document.querySelector("h2");
+placeHolderInList.style.display = "none";
 }
+
+
+//get rid of "nothing to do"//
+
+
 
 /*
 <div id="tasks">
